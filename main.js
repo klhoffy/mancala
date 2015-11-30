f395b399884ca3ed492ad63c1f5dfe40bcc98da2
Kristin Hoffer <kristinlhoffer@gmail.com>
2015-11-30 13:34:07 -0800

    Index setup

---
+++ main.js
@@ -0,0 +1,56 @@
+$(document).ready() {
+
+
+
+
+
+	function playerTurn() {
+
+	}
+
+	function checkBox() {
+		// if (box = empty) {
+		// 	player takes stones from isOpposite() + 1
+		// } else if (last stone = player mancala) {
+		// 	playerTurn() again
+		// } else {
+		// 	player turn over
+		// }
+
+	}
+
+	function checkAllBoxes() {
+		// if (".boxA" === 0) || (".boxB" === 0) {
+		// 	chooseWinner()
+		// }
+	}
+
+	function moveStones() {
+		// drops one stone in each respective box
+	}
+
+	function chooseWinner() {
+		// if ("#mancalaA" > "#mancalaB") {
+		// 	playerA wins
+		// } else {
+		// 	playerB wins
+		// }
+	}
+
+	function isOpposite() {
+		// box that is directly opposite
+
+	}
+
+	function gameInit() {
+		// box = 4 stones
+		// mancala = 0 stones
+		// playerTurn = playerA
+
+	}
+
+	function storeStonesInBox() {
+
+	}
+
+}
