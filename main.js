9c171177f582b61bc6940daf6acae1441d42e672
Kristin Hoffer <kristinlhoffer@gmail.com>
2015-12-01 16:51:04 -0800

    working game on console

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
 //$(document).ready() {
 
-	var board = [4,4,4,4,4,4,0,4,4,4,4,4,4,0]
+	var board = [4,4,2,4,0,4,0,4,4,4,4,4,4,0]
 	var playerOne = true;
-	var playerOne
-	
-	//var playerOne; //= board[0,1,2,3,4,5,6] //can only use these spots
-	//var playerTwo; //= board[7,8,9,10,11,12,13] //can only use these spots
-	// var players = ["playerOne", "playerTwo"]
 
-	// it finally works!
 	function play(box) { 
-		if (box === 6 || box === 13) {
-			alert("you cant take a stone from the mancala!")
-		} else {
-			var stones = board[box];
-			if (box === 0) { 
-				alert("there's nothing there")
+		if (playerOne && box < 6 && box > -1 ) {
+			if (box === 6 || box === 13) {
+				alert("you cant take a stone from the mancala!")
 			} else {
-				// if (playerOne = true) {
-				// 	board[box] = board[0] && board[1] && board[2] && board[3] && board[4] && board[5]
-				// } else {
-				// 	board[box] = board[7] && board[8] && board[9] && board[10] && board[11] && board[12]
-				// }
-				for (var i = 0; i <= stones; i++) {
-					board[(box + i) % 14] +=1
+				var stones = board[box];
+				if (board[box] === 0) { 
+					alert("there's nothing there")
+				} else {
+					for (var i = 0; i <= stones; i++) {
+						board[(box + i) % 14] +=1
+						if (box + i + stones === 6) {
+						playerOne = false
+						// } else if (board[box + i] + stones === 1) {
+						// 	isOpposite()
+						// 	console.log(box)
+						// }
+					}
 				}
+				board[box] = 0;
+				console.log(board)
+				checkAllBoxes()	
+			} 
+		} else if (!playerOne && box < 13 && box > 6 ) {
+			if (box === 6 || box === 13) {
+				alert("you cant take a stone from the mancala!")
+			} else {
+				var stones = board[box];
+				if (board[box] === 0) { 
+					alert("there's nothing there")
+				} else {
+					for (var i = 0; i <= stones; i++) {
+						board[(box + i) % 14] +=1
+						if (box + i + stones === 13) {
+						playerOne = true
+						}
+					}
+				}
+				board[box] = 0;
+				console.log(board)
+				checkAllBoxes()
 			}
-			board[box] = 0;
-			console.log(board)
-			checkAllBoxes()
-			
 		}
 	}
 
 	// works
 	function playerTurn() {
 		playerOne = !playerOne
-		console.log(playerOne)
-		
-
 	}
-
 
 	// it works
 	function checkAllBoxes() {
@@ -52,7 +63,6 @@
 			console.log("B cleared")
 		} else {
 			playerTurn()
-			console.log("new player turn")
 		}
 	} 
 
@@ -67,35 +77,22 @@
 		}
 	}
 
-	// this works
-	function isOpposite(box) {
-		if (box === 0) {
-			console.log(board[12])
-		} else if (box === 1) {
-			return board[11]
-		} else if (box === 2) {
-			return board[10]
-		} else if (box === 3) {
-			return board[9]
-		} else if (box === 4) {
-			return board[8]
-		} else if (box === 5) {
-			return board[7]
-		}
+	// function isOpposite(box) {
+	// 	if (board[box] === 0) {
+	// 		return board[12]
+	// 		console.log(board[12])
+	// 	} else if (box === 1) {
+	// 		return board[11]
+	// 	} else if (box === 2) {
+	// 		return board[10]
+	// 	} else if (box === 3) {
+	// 		return board[9]
+	// 	} else if (box === 4) {
+	// 		return board[8]
+	// 	} else if (box === 5) {
+	// 		return board[7]
+	// 	}
+	// }
 
-	}
-
-
-
-	function checkBoxEmpty() {
-		// if (box = empty) {
-		// 	player takes stones from isOpposite() + 1
-		// } else if (last stone = player mancala) {
-		// 	playerTurn() again
-		// } else {
-		// 	player turn over
-		// }
-
-	}
 
 //}
